0a157e46b64d014cbcafe04f19e6854cb4ad246a
jokinl <tesokarte@gmail.com>
2015-03-16 21:22:38 +0100

    delete/add
    delete js and creat new js

--- js/form.js
+++ js/form.js
@@ -21,24 +21,34 @@ FUNCTIONS
 // Open/Close form to publish Post
 function viewform(){
 	$("#js-form").slideToggle();
+	$(".article").slideToggle();
 }
 // Add Post to html
 function addpost(){
-	var titulo = $("#js-titulo").val(),
+	// Put in var inputs values
+	var title = $("#js-title").val(),
 		autor = $("#js-autor").val(),
 		tag = $("#js-tag").val(),
+		// New var with .article html
 		clone = post.clone();
-
+	// Find a and insert var title
 	clone.find(".article--title a")
-		.text(titulo);
+		.text(title);
 			/*.attr("href", url)*/
 	clone.find(".article--autor a")
 			.text(autor);
 	clone.find(".article--tag")
 			.text(tag);
 	clone.hide();
+	// Insert clone inside list in first position
 	list.prepend(clone);
-	clone.fadeIn();
-	
+	$("#js-form").slideToggle();
+	title = $("#js-title").val("");
+	autor = $("#js-autor").val("");
+	tag = $("#js-tag").val("");
+	$(".article").slideToggle();
+	/*$("#js-form input[type='text']").val("");*/
+	/*clone.fadeIn();*/
+
 	return false;
 }
